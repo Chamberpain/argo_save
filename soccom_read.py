62d80eeef137033fb62fadce4b19d324d948521e
Paul Chamberlain <paulfromthehc@gmail.com>
2017-11-01 14:00:40 -0700

    Changed the read subroutine to read netcdf files instead of text

--- soccom_read.py
+++ soccom_read.py
@@ -1,39 +1,28 @@
-import sys,os
-sys.path.append(os.path.abspath("../scripts"))
-sys.path.append(os.path.abspath("/Users/paulchamberlain/git/chamberpain-working/scripts"))
-sys.path.append(os.path.abspath("/Users/paulchamberlain/Data/"))
-import soccom_proj_settings
 import pandas as pd
-import itertools as it
 import glob
-time_format = '%m/%d/%Y'
 import numpy as np
-import soccom_proj_settings
+from netCDF4 import Dataset 
 
-SOCCOM_ids={'6091':'5904179','7557':'5904181','7567':'5904182','7613':'5904180','7614':'5904183',
-'9091':'5904184','9092':'5904185','9031':'5904396','9018':'5904186','9095':'5904188','9101':'5904187',
-'9254':'5904395','0037':'5904475','9313':'5904474','0508':'5904476','9096':'5904469','0509':'5904477',
-'7652':'5904467','0511':'5904478','9094':'5904471','9275':'5904472','9099':'5904468','9125':'5904397',
-'9260':'5904473','8514':'5904470','9668':'5904663','9666':'5904662','9646':'5904661','9652':'5904660',
-'9657':'5904659','9655':'5904658','9662':'5904657','0506':'5904670','0507':'5904671','9749':'5904675',
-'9645':'5904676','9757':'5904679','0564':'5904687','0510':'5904686','9602':'5904684','9637':'5904682',
-'9650':'5904683','9600':'5904688','9631':'5904677','9744':'5904678','0570':'5904768','0568':'5904767',
-'0565':'5904672','9761':'5904764','0571':'5904673','9265':'5904695','0566':'5904766','9660':'5904761',
-'9632':'5904763','9634':'5904693','9762':'5904765','9630':'5904674','9752':'5904694','0068':'5903717',
-'5146':'5901492','5426':'5902112','6968':'5903718','7552':'5903729','7619':'5904105','7620':'5904104',
-'6091':'5904179'}
-
-
-def skiplines_finder(file_):    #this algorythm iterates through the file until it comes to a specific formatting - all SOCCOM data begins in the text file with "CRUISE"
-    with open(file_) as f:
-        g = it.takewhile(lambda x: x[:6] != 'Cruise', f)
-        skip_number = len([i for i, _ in enumerate(g)])+1
-        return skip_number
-
-def soccom_file_reader(file_):
-    col = [u'Cruise', u'Date',  u'Lon', u'Lat', 'PosQC', u'Pressure','PressureQC',u'Temperature','TempQC',u'Salinity','SalQC', u'Oxygen','OxygenQC','OxygenSat','OxygenSatQC', u'Nitrate','NitrateQC','PH','PHQC','TALK','TALKQC','DIC','DICQC','pCO2','pCO2QC']   #initialize the column names
-    skip_number = skiplines_finder(file_)   #get the number of lines to skip in the beginning of the record
-    holder = pd.read_csv(file_, sep='\t',error_bad_lines=False,names = col, skiprows= skip_number, warn_bad_lines=False,usecols = [0,3,5,6,7,8,9,10,11,12,13,18,19,20,21,22,23,36,37,38,39,40,41,42,43], na_values='-1E+10')    #make a data frame and return it
+def soccom_file_reader(file_token):
+    frame_token = []
+    col = [u'Pressure','Pressure_QF',u'Temperature','Temperature_QF',u'Salinity','Salinity_QF', u'Oxygen','Oxygen_QF','OxygenSat','OxygenSat_QF', u'Nitrate','Nitrate_QF','pH25C','pH25C_QF']   #initialize the column names
+    nc_token = Dataset(file_token)
+    cruise = ''.join(nc_token['Cruise'][:].tolist()).strip()
+    for date_header in range(nc_token.dimensions['N_PROF'].size):
+        date = ''.join(nc_token['mon_day_yr'][date_header].tolist())
+        Lon = nc_token['Lon'][date_header]
+        Lat = nc_token['Lat'][date_header]
+        PosQC = nc_token['Lat_QF'][date_header]
+        dataframe_token = pd.DataFrame()
+        for variable in col: 
+            dataframe_token[variable]=nc_token[variable][date_header]
+        dataframe_token['Cruise']=cruise
+        dataframe_token['Date']=pd.to_datetime(date)
+        dataframe_token['Lat']=Lat
+        dataframe_token['Lon']=Lon
+        dataframe_token['PosQC']=PosQC
+        frame_token.append(dataframe_token)
+    holder = pd.concat(frame_token)
     return holder
 
 def soccom_df(data_directory):
@@ -43,7 +32,6 @@ def soccom_df(data_directory):
         print float_name
         frames.append(soccom_file_reader(float_name))           #pandas is orders of magnitude faster at appending then concatenating
     df_holder = pd.concat(frames)
-    df_holder = df_holder[df_holder.Lat<-50]
     df_holder.Date = pd.to_datetime(df_holder.Date,format=time_format)  #make all datetime objects
     df_holder = df_holder.dropna(subset=['Pressure'])
     df_holder = df_holder.dropna(subset = ['Date'])     #drop bad dates (nothing can be done with this data)
@@ -52,15 +40,12 @@ def soccom_df(data_directory):
     df_holder.loc[df_holder.PosQC==4,'PosQC']=8
     df_holder.loc[df_holder.PosQC==0,'PosQC']=1
     df_holder.loc[df_holder.PressureQC!=0,'Pressure']=np.nan
-    df_holder.loc[df_holder.TempQC!=0,'Temperature']=np.nan
-    df_holder.loc[df_holder.SalQC!=0,'Salinity']=np.nan
-    df_holder.loc[df_holder.OxygenQC!=0,'Oxygen']=np.nan
-    df_holder.loc[df_holder.OxygenSatQC!=0,'OxygenSat']=np.nan
-    df_holder.loc[df_holder.NitrateQC!=0,'Nitrate']=np.nan
-    df_holder.loc[df_holder.PHQC!=0,'PH']=np.nan
-    df_holder.loc[df_holder.TALKQC!=0,'TALK']=np.nan
-    df_holder.loc[df_holder.DICQC!=0,'DIC']=np.nan
-    df_holder.loc[df_holder.pCO2QC!=0,'pCO2']=np.nan
+    df_holder.loc[df_holder.Temperature_QF!=0,'Temperature']=np.nan
+    df_holder.loc[df_holder.Salinity_QF!=0,'Salinity']=np.nan
+    df_holder.loc[df_holder.Oxygen_QF!=0,'Oxygen']=np.nan
+    df_holder.loc[df_holder.OxygenSat_QF!=0,'OxygenSat']=np.nan
+    df_holder.loc[df_holder.Nitrate_QF!=0,'Nitrate']=np.nan
+    df_holder.loc[df_holder.pH25C_QF!=0,'pH25C']=np.nan
     df_holder['Type']='SOCCOM'
     df_holder = df_holder.drop_duplicates(subset=['Cruise','Date','Pressure'])
     df_holder = df_holder.sort_values(['Cruise','Date','Pressure'])    #sort in a reasonable manner
