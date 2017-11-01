83b334786c4ee34198cbe9648e020277f0070aa4
Paul Chamberlain <paulfromthehc@gmail.com>
2017-11-01 16:12:38 -0700

    Fixed a bug that checked the value 0 versus the string '0'

--- soccom_read.py
+++ soccom_read.py
@@ -32,24 +32,24 @@ def soccom_df(data_directory):
         print float_name
         frames.append(soccom_file_reader(float_name))           #pandas is orders of magnitude faster at appending then concatenating
     df_holder = pd.concat(frames)
-    df_holder.Date = pd.to_datetime(df_holder.Date,format=time_format)  #make all datetime objects
     df_holder = df_holder.dropna(subset=['Pressure'])
     df_holder = df_holder.dropna(subset = ['Date'])     #drop bad dates (nothing can be done with this data)
     df_holder.loc[df_holder.PosQC==1,'Lon']=np.nan
     df_holder.loc[df_holder.PosQC==1,'Lat']=np.nan
     df_holder.loc[df_holder.PosQC==4,'PosQC']=8
     df_holder.loc[df_holder.PosQC==0,'PosQC']=1
-    df_holder.loc[df_holder.PressureQC!=0,'Pressure']=np.nan
-    df_holder.loc[df_holder.Temperature_QF!=0,'Temperature']=np.nan
-    df_holder.loc[df_holder.Salinity_QF!=0,'Salinity']=np.nan
-    df_holder.loc[df_holder.Oxygen_QF!=0,'Oxygen']=np.nan
-    df_holder.loc[df_holder.OxygenSat_QF!=0,'OxygenSat']=np.nan
-    df_holder.loc[df_holder.Nitrate_QF!=0,'Nitrate']=np.nan
-    df_holder.loc[df_holder.pH25C_QF!=0,'pH25C']=np.nan
+    df_holder.loc[df_holder.Pressure_QF!='0','Pressure']=np.nan
+    df_holder.loc[df_holder.Temperature_QF!='0','Temperature']=np.nan
+    df_holder.loc[df_holder.Salinity_QF!='0','Salinity']=np.nan
+    df_holder.loc[df_holder.Oxygen_QF!='0','Oxygen']=np.nan
+    df_holder.loc[df_holder.OxygenSat_QF!='0','OxygenSat']=np.nan
+    df_holder.loc[df_holder.Nitrate_QF!='0','Nitrate']=np.nan
+    df_holder.loc[df_holder.pH25C_QF!='0','pH25C']=np.nan
     df_holder['Type']='SOCCOM'
     df_holder = df_holder.drop_duplicates(subset=['Cruise','Date','Pressure'])
     df_holder = df_holder.sort_values(['Cruise','Date','Pressure'])    #sort in a reasonable manner
     df_holder = df_holder.reset_index(drop=True)
+    df_holder = df_holder[['Date','Lat','Lon','Cruise','PosQC','Pressure','Temperature','Salinity','Oxygen','OxygenSat','Nitrate','pH25C']]
     return df_holder
 
 
