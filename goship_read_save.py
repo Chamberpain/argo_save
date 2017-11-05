8c492aa60c4596e4ca86a3e81c07d56fbd62efde
Paul Chamberlain <paulfromthehc@gmail.com>
2017-11-04 18:43:44 -0700

    Added comments and saving

--- goship_read_save.py
+++ goship_read_save.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 matches = []
 frames = []
-variable_list = ['pressure','salinity','temperature','oxygen','nitrate','alkalinity']
+variable_list = ['pressure','salinity','temperature','oxygen','nitrate','alkalinity'] #ph is temperature dependent, and i am too lazy to do the correction
 
 def goship_file_reader(file_):
 	nc_fid = Dataset(file_, 'r')
@@ -30,39 +30,31 @@ def goship_file_reader(file_):
 	try: 
 		df_token['Date']=datetime.strptime(str(nc_fid['woce_date'][:][0]),'%Y%m%d')
 	except ValueError:
-		df_token['Date'] = np.nan
+		df_token['Date'] = np.nan #there are a few profile files without reasonable dates
 	except IndexError:
-		df_token['Date'] = pd.to_datetime(datetime.strptime(nc_fid.Created,'%Y%m%d %I:%S').date())
+		df_token['Date'] = pd.to_datetime(datetime.strptime(nc_fid.Created,'%Y%m%d %I:%S').date()) # not all the net cdf files are in the same format, this normalizes them
 	try:
 		df_token['Cruise']=nc_fid.WOCE_ID
 	except AttributeError:
-		df_token['Cruise']=nc_fid.WHP_SECTION_ID
-	df_token = df_token.dropna(subset = ['Date','Pressure'])
+		df_token['Cruise']=nc_fid.WHP_SECTION_ID # not all net cdf files have the same naming convention, this references the correct one
+	df_token = df_token.dropna(subset = ['Date','Pressure']) #without date or pressure, these measurements are useless...
 	nc_fid.close()
 	return df_token
 
 
 
 for root, dirnames, filenames in os.walk(soccom_proj_settings.goship_data_directory):
-    for filename in fnmatch.filter(filenames, '*.nc'):
+    for filename in fnmatch.filter(filenames, '*.nc'): #find all net cdf files in the specified directory
         matches.append(os.path.join(root, filename))
 
 for n, match in enumerate(matches):
-	if (i % 100) ==0:   
-    	print 'file is ',match,', there are ',len(matches[:])-n,'goship profiles left'
-    frames.append(goship_file_reader(match))
+	if (n % 100) ==0:   # print every 100th value, so we know where we are and how long it will take 
+		print 'file is ',match,', there are ',len(matches[:])-n,'goship profiles left'
+	frames.append(goship_file_reader(match))
 
-df_holder = pd.concat(frames)
+df_holder = pd.concat(frames) #merge all dataframes together
 df_holder['Type']='GOSHIP'
-
-
-
-
-
-
-
-
-
-
-
+df_holder = df_holder[['Cruise','Date','Temperature','Salinity','Oxygen','Nitrate','Alkalinity','Lat','Lon']]
+df_holder = df_holder.sort_values(['Cruise','Date','Pressure'])    #sort in a reasonable manner
+df_holder.to_pickle(soccom_proj_settings.goship_file)
 
