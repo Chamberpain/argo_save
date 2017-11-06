1c9a797a14657dce049a035e2086aeb339767538
Paul Chamberlain <paulfromthehc@gmail.com>
2017-11-06 09:53:37 -0800

    Cut off values north of 40

--- goship_read_save.py
+++ goship_read_save.py
@@ -57,6 +57,7 @@ for n, match in enumerate(matches):
 	frames.append(goship_file_reader(match))
 
 df_holder = pd.concat(frames) #merge all dataframes together
+df_holder = df_holder[df_holder.Lat<-40]
 df_holder['Type']='GOSHIP'
 df_holder = df_holder[['Cruise','Date','Temperature','Salinity','Oxygen','Nitrate','Alkalinity','Pressure','Lat','Lon']]
 df_holder = df_holder.sort_values(['Cruise','Date','Pressure'])    #sort in a reasonable manner
